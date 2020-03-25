627cf14e309bb56555c3746ec1364dd38fd0a10b
llow <low.lucyy@gmail.com>
2020-03-25 12:26:34 -0400

    Update YaoPlots.jl

--- src/YaoPlots.jl
+++ src/YaoPlots.jl
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-# Plots a demo quantum circuit in julia with compose julia framework 
-# Greenberger-Horne-Zeilinger (GHZ) state with Quantum Circuit 
-
-# import Pkg; Pkg.add("Yao")
-# import Pkg; Pkg.add("Compose")
-# using Compose
-# using Yao, Compose, Colors, Measures
-
-=======
->>>>>>> parent of e6e03ee... Update YaoPlots.jl
 module YaoPlots
-
 export plot
 using Yao, Compose, Colors, Measures
 
